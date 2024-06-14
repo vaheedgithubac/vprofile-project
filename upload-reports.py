51736643e382e7f89b99bcc5ea2251a1e31d4b92
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2024-06-14 17:04:58 +0530

    Create upload-reports.py

---
+++ upload-reports.py
@@ -0,0 +1,44 @@
+import requests
+import sys
+
+filename = sys.argv[1]
+scan_type = ''
+
+if file_name == 'gitleaks.json':
+    scan_type = 'Gitleaks Scan'
+elif file_name == 'trivyfs.json' or file_name == 'trivyimg.json':
+    scan_type = 'Trivy Scan'
+elif file_name == 'owaspdc.xml':
+    scan_type = 'Dependency Check Scan'    
+
+headers = {
+  'Authorization' : 'Token 548afd6fab3bea9794a41b31da0e9404f733e222'
+}
+
+url = 'https://demo.defectdojo.org/api/v2/import-scan/'
+
+data = {
+  'active': True,
+  'verified': True,
+  'scan_type': scan_type,
+  'minimum_severity': 'Low',
+  'engagement': 18
+}
+
+files = {
+  'file': open(filename, 'rb')
+}
+
+response = requests.post(url, headers=headers, data=data, files=files)
+
+if response.status_code ==201:
+  
+  # print(filename, "Scan results uploaded to DEFECT-DOJO successfully...!!")
+  # print(f"{filename}  Scan results uploaded to DEFECT-DOJO successfully...!!")
+
+  print("{} Scan results uploaded to DEFECT-DOJO successfully...!!".format(filename))
+
+else:
+  print(f'Failed to upload scan results: {response.content}') 
+
+
